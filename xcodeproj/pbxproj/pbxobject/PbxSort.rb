100a758d387d5a58a91aa9845b76a62f6ba7de94
keisuke <jam330157@gmail.com>
2018-01-26 18:53:14 +0900

    refactoring PBXSort.rb

--- xcodeproj/pbxproj/pbxobject/PbxSort.rb
+++ xcodeproj/pbxproj/pbxobject/PbxSort.rb
@@ -6,9 +6,6 @@ class PbxSort
 
     def psort
 
-        # pbxsort = @pbxs.sort{|pbxp1,pbxp2|
-        #     pbxp1.partition <=> pbxp2.parentName
-        # }
         sorteds = Array.new
         @pbxs.each{ |pbx|
             # puts "parent PBX"
@@ -33,10 +30,6 @@ class PbxSort
                 # puts pbx.name
                 # ->> puts pbx.childPbx
             }
-            # puts "\nArrays\n"
-            # child.each{|p|
-            #     puts p
-            # }
             sorteds.push(childrenSort(pbx.pbxBase,child))
             # pbx.children.each {|pbx|
             #
@@ -50,37 +43,29 @@ class PbxSort
     end
 
     def childrenSort (children, sort)
-        # puts "\n\nchildSort\n\n"
         pbx = ""
         pbxSplit = children.split("\n")
         isPbxChild = false
         num = 0
         pbxSplit.each{|pbx_line|
 
-            # puts pbx_line
-
             if pbx_line.index(");")
-                # puts "FALSE HIT"
                 isPbxChild = false
             end
 
             if isPbxChild == true
                 pbx_line = sort[num]
-                # puts "pbx_line #{pbx_line}"
                 num += 1
             else
                 pbx_line << "\n"
             end
 
-            # # Children
+            # Children
             if pbx_line.index("children = (")
-                # puts "HIT"
                 isPbxChild = true
             end
             pbx << pbx_line
         }
-        # puts "\n\nPBXSORT\n\n"
-        # puts pbx
         return pbx
     end
 end
